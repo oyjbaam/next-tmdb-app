65c9de6894c12185369df7e16fd263bbe1ceeb89
oyjbaam <oyjbaam@gmail.com>
2024-06-15 17:20:30 +0900

    feat: 홈 이미지 캐러셀로 수정

--- components/main/mainImage.tsx
+++ components/main/mainImage.tsx
@@ -1,30 +1,36 @@
 import Image from 'next/image'
-import { getRandomNumber } from '@/shared/util/getRandomNumber'
 import { getMainBannerImg } from '@/shared/api/tmdbAPI'
+import { CarouselItem } from '../ui/carousel'
+import CarouselWrapper from './CarouselWrapper'
 
 const MainImage = async () => {
   const backDrop = await getMainBannerImg()
-  const randomNum = getRandomNumber(0, backDrop.results.length - 1)
-  const imgUrl = `https://image.tmdb.org/t/p/w780${backDrop.results[randomNum].backdrop_path}`
 
   return (
-    <div className="relative rounded-xl w-full h-80 overflow-hidden">
-      <div className="absolute w-full h-full z-10 flex flex-col justify-center px-10 text-white">
-        <h2 className="text-5xl font-extrabold">Test deploy</h2>
-        <h3 className="text-2xl font-extrabold">Millions of Movies, Tv Shows and People to discover.</h3>
-      </div>
-      <div className="absolute w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500">
-        <Image
-          src={imgUrl}
-          alt="main page image"
-          fill
-          className="object-cover object-top opacity-40 saturate-50"
-          priority
-          decoding="async"
-          fetchPriority="high"
-        />
-      </div>
-    </div>
+    <CarouselWrapper>
+      {backDrop.results.map((item, index) => {
+        const imgSrc = `https://image.tmdb.org/t/p/w780${item.backdrop_path}`
+        return (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <div>
+                <div className="relative w-full h-80 flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md overflow-hidden">
+                  <Image
+                    src={imgSrc}
+                    alt="main page image"
+                    fill
+                    className="object-cover object-top opacity-40 saturate-50"
+                    priority
+                    decoding="async"
+                    fetchPriority="high"
+                  />
+                </div>
+              </div>
+            </div>
+          </CarouselItem>
+        )
+      })}
+    </CarouselWrapper>
   )
 }
 
--- components/ui/button/iconButton.tsx
+++ components/ui/button/iconButton.tsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react'
 import { ButtonProps, buttonStyles, SVGComponent } from './button'
 import { cn } from '@/shared/util/twMerge'
-type IconButtonProps = Omit<ButtonProps, 'rounded' | 'leadingIcon' | 'trailingIcon' | 'children'> & {
-  icon: SVGComponent
+type IconButtonProps = Omit<ButtonProps, 'rounded' | 'leadingIcon' | 'trailingIcon'> & {
+  icon?: SVGComponent
 }
 export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-  ({ icon: Icon, intent, sizes, disabled, className, ...props }: IconButtonProps, ref) => {
+  ({ icon: Icon, intent, sizes, disabled, className, children, ...props }: IconButtonProps, ref) => {
     return (
       <button
         className={cn(
@@ -22,7 +22,7 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
         {...props}
         ref={ref}
       >
-        <Icon className="w-5 h-5" aria-hidden="true" />
+        {children ? children : Icon && <Icon className="w-5 h-5" aria-hidden="true" />}
       </button>
     )
   }
