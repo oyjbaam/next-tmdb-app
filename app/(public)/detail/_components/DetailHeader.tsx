5024910e11069ac9db07a8045da3e181e3876256
oyjbaam <oyjbaam@gmail.com>
2024-06-17 17:15:58 +0900

    feat: 디테일 페이지 컨텐츠 헤더

---
+++ app/(public)/detail/_components/DetailHeader.tsx
@@ -0,0 +1,55 @@
+import { MovieDetailType, TvDetailType } from '@/shared/types'
+import { isMovieDetailTypeGuard } from '@/shared/util/guard/isMovieDetailTypeGuard'
+import React from 'react'
+import { StarIcon } from 'lucide-react'
+import FlexBox from '@/components/ui/FlexBox'
+type DetailHeaderProps = {
+  data: MovieDetailType | TvDetailType
+}
+
+const DetailHeader = ({ data }: DetailHeaderProps) => {
+  const isMovie = isMovieDetailTypeGuard(data, 'movie')
+
+  return (
+    <header className="space-y-4">
+      <div>
+        <h1 className="text-6xl font-bold">{(isMovie ? data.original_title : data.original_name).toUpperCase()}</h1>
+        <h2 className="text-3xl mt-1">{(isMovie ? data.title : data.name).toUpperCase()}</h2>
+      </div>
+
+      <ul className="flex *:relative gap-4 mt-4 text-base *:ml-2 *:before:content-[''] *:before:block *:before:absolute *:before:w-1 *:before:aspect-square *:before:rounded-full *:before:bg-slate-400 *:before:-left-2 *:before:top-[10px]">
+        <li>
+          <span>{isMovie ? data.release_date : data.first_air_date}</span>
+        </li>
+        <li>{isMovie ? <span>{data.runtime} min</span> : <span> {data.number_of_episodes} Episodes</span>}</li>
+        <li>
+          <span>{data.original_language.toUpperCase()}</span>
+        </li>
+      </ul>
+
+      <FlexBox alignItems="center" className="gap-2 text-yellow-400">
+        <StarRating voteAverage={data.vote_average} />
+        <span className="text-base">{data.vote_average.toFixed(1)}</span>
+      </FlexBox>
+    </header>
+  )
+}
+
+export default DetailHeader
+
+type StarRatingProps = {
+  voteAverage: number
+}
+
+const StarRating = ({ voteAverage }: StarRatingProps) => {
+  const rating = Math.round(voteAverage) / 2
+  const stars = Array.from({ length: 5 }, (_, index) => index + 1)
+
+  return (
+    <FlexBox className="gap-2">
+      {stars.map(star => {
+        return <StarIcon className="w-5 h-5" key={star} fill={rating >= star ? 'gold' : 'none'} />
+      })}
+    </FlexBox>
+  )
+}
--- app/(public)/detail/page.tsx
+++ app/(public)/detail/page.tsx
@@ -1,37 +1,42 @@
 import React from 'react'
 import { getDetail } from '@/shared/api/tmdbAPI'
 import Image from 'next/image'
+import { isMovieDetailTypeGuard } from '@/shared/util/guard/isMovieDetailTypeGuard'
+import DetailHeader from './_components/DetailHeader'
 
 type DetailPageProps = {
-  searchParams: Record<string, string | string[] | undefined>
+  searchParams: Record<string, string | undefined>
 }
 
 const DetailPage = async ({ searchParams }: DetailPageProps) => {
-  const mediaType = searchParams.mediaType ?? ''
+  const mediaType = searchParams.mediaType as 'movie' | 'tv'
   const dataId = searchParams.id ?? ''
   const fetchUrl = `/${mediaType}/${dataId}`
-  const detailData = await getDetail(fetchUrl)
+  const detailData = await getDetail<typeof mediaType>(fetchUrl)
 
   const imgPath = `https://image.tmdb.org/t/p/w500${detailData.poster_path}` ?? '/images/defaultPosterImage.png'
+  const isMovie = isMovieDetailTypeGuard(detailData, 'movie')
 
   return (
-    <div>
-      <div className="grid grid-cols-2">
-        <div className="relative h-[750px] w-[500px] rounded-md overflow-hidden">
-          <Image
-            src={imgPath}
-            alt={detailData.name || detailData.title || '포스터 이미지'}
-            fill
-            sizes="auto"
-            className="object-cover"
-            title={detailData.name || detailData.title || '포스터 이미지'}
-          />
-        </div>
+    <>
+      <section className="relative h-[750px] w-[500px] rounded-md overflow-hidden">
+        <Image
+          src={imgPath}
+          alt={(isMovie ? detailData.title : detailData.name) ?? '포스터 이미지'}
+          fill
+          sizes="auto"
+          className="object-cover"
+          title={(isMovie ? detailData.title : detailData.name) ?? '포스터 이미지'}
+        />
+      </section>
+      <section>
+        <DetailHeader data={detailData} />
+
         <div>
-          <h2 className="text-3xl font-bold">{detailData.title || detailData.name}</h2>
+          <h3>The CAST</h3>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   )
 }
 
