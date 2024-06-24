bdc8557b691d6452889eeefd310b0c3c66c85bd9
oyjbaam <oyjbaam@gmail.com>
2024-06-24 23:19:18 +0900

    feat: 인증토큰 모델 및 생성 함수 작성

---
+++ shared/data/verificationToken.ts
@@ -0,0 +1,22 @@
+import { db } from '../db'
+
+/**
+ * @returns email로 db에서 인증 토큰 찾아 리턴
+ */
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({ where: { email } })
+    return verificationToken
+  } catch {
+    return null
+  }
+}
+
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findUnique({ where: { token } })
+    return verificationToken
+  } catch {
+    return null
+  }
+}
