b57dd311484ff2e74c20c6c4f10e5de5b8bc3485
oyjbaam <oyjbaam@gmail.com>
2024-06-27 23:12:16 +0900

    refactor: home 메인 이미지 영역 페이지 컴포넌트 재작성

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 type InnerLayoutProps = {
   children: React.ReactNode
-  homemainimage: React.ReactNode
   whatspopular: React.ReactNode
   trending: React.ReactNode
 }
 
-const InnerLayout = ({ children, homemainimage, whatspopular, trending }: InnerLayoutProps) => {
+const InnerLayout = ({ children, whatspopular, trending }: InnerLayoutProps) => {
   return (
     <div className="inner px-3">
       {children}
-      {homemainimage}
       {whatspopular}
       {trending}
     </div>
---
+++ app/(home)/loading.tsx
@@ -0,0 +1,32 @@
+import CardSkeleton from '@/components/common/cardSkeleton'
+import FlexBox from '@/components/ui/FlexBox'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div role="status" className=" animate-pulse">
+      <div className="h-80 w-full bg-gray-200 dark:bg-slate-700 rounded-md"></div>
+      <div className="animate-pulse mt-10 ">
+        <div className="h-10 lg:w-[400px] w-full bg-slate-200 dark:bg-slate-700 rounded-md mb-4"></div>
+        <FlexBox className="w-full overflow-hidden gap-4">
+          {[...Array(5)].map((_, i) => (
+            <CardSkeleton key={i} />
+          ))}
+        </FlexBox>
+      </div>
+      <div className="animate-pulse mt-10 ">
+        <div className="h-10 lg:w-[400px] w-full bg-slate-200 dark:bg-slate-700 rounded-md mb-4"></div>
+        <FlexBox className="w-full overflow-hidden gap-4">
+          {[...Array(5)].map((_, i) => (
+            <CardSkeleton key={i} />
+          ))}
+        </FlexBox>
+      </div>
+      <span role="status" className="sr-only">
+        loading...
+      </span>
+    </div>
+  )
+}
+
+export default loading
