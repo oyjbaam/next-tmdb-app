64863947599830fb11bae8579568e98fab323712
oyjbaam <oyjbaam@gmail.com>
2024-07-25 18:30:55 +0900

    feat: 나의 목록 페이지 데이터 페치

---
+++ app/(protected)/my-list/[list-id]/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const MyListDetailPage = () => <div>MyListDetailPage</div>
+
+export default MyListDetailPage
--- app/(protected)/my-list/page.tsx
+++ app/(protected)/my-list/page.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { getLikedList } from '@/shared/data'
+import { getMyListById } from '@/shared/data'
 import { getcurrentUser } from '@/shared/lib/getCurrentUser'
 import Grid from '@/components/common/Grid'
-import Card from '@/components/common/card/Card'
 import Link from 'next/link'
 import FlexBox from '@/components/ui/FlexBox'
 
 const MyListPage = async () => {
   const user = await getcurrentUser()
-  const likedList = await getLikedList(user?.id)
+  const myList = await getMyListById(user?.id)
 
-  if (!likedList || likedList.length === 0) {
+  if (!myList || myList.length === 0) {
     return (
       <FlexBox alignItems="center" justifyContent="center">
         <h1 className="text-2xl">👉 좋아요 리스트가 없습니다</h1>
@@ -20,15 +19,11 @@ const MyListPage = async () => {
 
   return (
     <Grid>
-      {likedList?.map(item => {
+      {myList.map(item => {
         {
           return (
-            <Link
-              key={item.tmdbId}
-              href={`/detail?mediaType=${item.mediaType}&id=${item.tmdbId}`}
-              className="w-full h-full"
-            >
-              <Card data={item} />
+            <Link key={item.id} href={`/my-list/${item.id}`} className="w-full h-full">
+              <span>{item.name}</span>
             </Link>
           )
         }
